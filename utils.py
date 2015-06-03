a22400263c3a6cd237493c71d28f7e0da6428fad
Mark Tozzi <mark.tozzi@gmail.com>
2015-06-02 20:36:47 -0400

    Made box class play a little nicer

--- utils.py
+++ utils.py
@@ -23,4 +23,20 @@ class Box(object):
         self.bottom = bottom
         self.right = right
 
+    def __str__(self):
+        return ("Box(top=%s, left=%s, bottom=%s, right=%s)" %
+               (self.top, self.left, self.bottom, self.right))
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __len__(self):
+        return 4
+
+    def __iter__(self):
+        yield self.top
+        yield self.left
+        yield self.bottom
+        yield self.right
+
 Point = namedtuple('Point', ['x', 'y'])
